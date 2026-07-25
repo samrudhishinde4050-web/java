021a7f3025e008da400a1d47cf6906584ac08c23
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-07-25 17:59:57 +0530

    DataTypes.java
    Added Java example for primitive data types with sample values and output.

---
+++ Variables-DataTypes/DataTypes.java
@@ -0,0 +1,23 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        byte b = 50;
+        short s = 700;
+        int i = 8000;
+        long l = 300000L;
+        float f = 24.5f;
+        double d = 54.78;
+        char c = 'B';
+        boolean flag = true;
+
+        System.out.println("Byte: " + b);
+        System.out.println("Short: " + s);
+        System.out.println("Int: " + i);
+        System.out.println("Long: " + l);
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+        System.out.println("Char: " + c);
+        System.out.println("Boolean: " + flag);
+    }
+}

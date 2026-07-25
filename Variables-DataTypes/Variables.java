ac1b4c93852022c3c683474e97302048acbf4835
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-07-25 17:50:53 +0530

    Variables.java example

---
+++ Variables-DataTypes/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+        int age = 20;
+        String name = "Sayali";
+        double salary = 25000.50;
+
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("Salary : " + salary);
+
+    }
+
+}

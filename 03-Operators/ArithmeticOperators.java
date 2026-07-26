116fd7129cf1cdccb1d8583fc83aff7a83e40589
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-07-26 11:50:55 +0530

    ArithmeticOperators.java

---
+++ 03-Operators/ArithmeticOperators.java
@@ -0,0 +1,16 @@
+public class ArithmeticOperators {
+
+    public static void main(String[] args) {
+
+        int a = 30;
+        int b = 10;
+
+        System.out.println("Addition = " + (a + b));
+        System.out.println("Subtraction = " + (a - b));
+        System.out.println("Multiplication = " + (a * b));
+        System.out.println("Division = " + (a / b));
+        System.out.println("Modulus = " + (a % b));
+
+    }
+
+}

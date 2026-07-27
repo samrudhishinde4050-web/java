3989152f19528451f9f47ec0378acc827654f429
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-07-27 14:58:17 +0530

    AssignmentOperators example

---
+++ 03-Operators/AssignmentOperators.java
@@ -0,0 +1,22 @@
+public class AssignmentOperators {
+
+    public static void main(String[] args) {
+
+        int a = 20;
+
+        a += 5;
+        System.out.println("+= : " + a);
+
+        a -= 2;
+        System.out.println("-= : " + a);
+
+        a *= 3;
+        System.out.println("*= : " + a);
+
+        a /= 2;
+        System.out.println("/= : " + a);
+
+        a %= 4;
+        System.out.println("%= : " + a);
+    }
+}

aa561c4433d5f47d1aa6ec2b025f5647c2565a8c
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-07-27 15:04:23 +0530

    UnaryOperators.java

---
+++ 03-Operators/UnaryOperators.java
@@ -0,0 +1,10 @@
+public class UnaryOperators {
+
+    public static void main(String[] args) {
+
+        int a = 20;
+
+        System.out.println("+a = " + (+a));
+        System.out.println("-a = " + (-a));
+    }
+}

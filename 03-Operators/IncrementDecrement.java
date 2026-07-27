bf3b60e2aafac879de43da9768c7e41b1ac6ff4d
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-07-27 15:04:51 +0530

    IncrementDecrement.java

---
+++ 03-Operators/IncrementDecrement.java
@@ -0,0 +1,19 @@
+public class IncrementDecrement {
+
+    public static void main(String[] args) {
+
+        int a = 50;
+
+        System.out.println("Initial Value = " + a);
+
+        System.out.println("Post Increment = " + (a++));
+        System.out.println("Current Value = " + a);
+
+        System.out.println("Pre Increment = " + (++a));
+
+        System.out.println("Post Decrement = " + (a--));
+        System.out.println("Current Value = " + a);
+
+        System.out.println("Pre Decrement = " + (--a));
+    }
+}

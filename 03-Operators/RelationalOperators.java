16b2626fda4f72a1f5fac7e8d9a6f8016b17dca1
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-07-27 14:59:30 +0530

    RelationalOperators example

---
+++ 03-Operators/RelationalOperators.java
@@ -0,0 +1,15 @@
+public class RelationalOperators {
+
+    public static void main(String[] args) {
+
+        int a = 30;
+        int b = 20;
+
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a != b : " + (a != b));
+        System.out.println("a > b : " + (a > b));
+        System.out.println("a < b : " + (a < b));
+        System.out.println("a >= b : " + (a >= b));
+        System.out.println("a <= b : " + (a <= b));
+    }
+}

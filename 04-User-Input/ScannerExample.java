46b48840e68a0edac44d628b49e2d9099196f1e6
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-07-27 14:55:52 +0530

    ScannerExample.java

---
+++ 04-User-Input/ScannerExample.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ScannerExample {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        System.out.println("You entered: " + number);
+
+        sc.close();
+    }
+}

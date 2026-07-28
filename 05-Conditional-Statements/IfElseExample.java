b6ba14392e8f7f1f873e877008e304764b052a20
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-07-28 09:24:00 +0530

    IfElseExample.java

---
+++ 05-Conditional-Statements/IfElseExample.java
@@ -0,0 +1,14 @@
+public class IfElseExample {
+
+    public static void main(String[] args) {
+
+        int marks = 35;
+
+        if(marks >= 40){
+            System.out.println("Pass");
+        }else{
+            System.out.println("Fail");
+        }
+
+    }
+}

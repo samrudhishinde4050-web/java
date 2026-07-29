3b82b7ff48e50caedc1ce03c102cf68e28b3e1a5
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-07-29 16:25:30 +0530

    BreakExample.java

---
+++ 06-Loops/BreakExample.java
@@ -0,0 +1,19 @@
+public class BreakExample {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+
+            if (i == 6) {
+
+                break;
+
+            }
+
+            System.out.println(i);
+
+        }
+
+    }
+
+}

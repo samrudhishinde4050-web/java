4698d0734a5261382c2b6902e0248f5e103ba387
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-07-29 16:27:28 +0530

    PrintNumbers1To100.java

---
+++ 06-Loops/PrintNumbers1To100.java
@@ -0,0 +1,13 @@
+public class PrintNumbers1To100 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+
+            System.out.println(i);
+
+        }
+
+    }
+
+}

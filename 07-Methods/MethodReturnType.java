fc8be909ee0149d7d7ff360d46cbf41d7fd07cbf
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-07-30 20:54:07 +0530

    MethodReturnType.java

---
+++ 07-Methods/MethodReturnType.java
@@ -0,0 +1,17 @@
+public class MethodReturnType {
+
+    static int square(int number) {
+
+        return number * number;
+
+    }
+
+    public static void main(String[] args) {
+
+        int result = square(5);
+
+        System.out.println("Square = " + result);
+
+    }
+
+}

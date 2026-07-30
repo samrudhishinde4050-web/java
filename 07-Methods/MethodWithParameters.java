785949604dc7bf627d799bc41aae1e66f3808229
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-07-30 20:53:24 +0530

    MethodWithParameters.java

---
+++ 07-Methods/MethodWithParameters.java
@@ -0,0 +1,15 @@
+public class MethodWithParameters {
+
+    static void add(int a, int b) {
+
+        System.out.println("Sum = " + (a + b));
+
+    }
+
+    public static void main(String[] args) {
+
+        add(10, 20);
+
+    }
+
+}

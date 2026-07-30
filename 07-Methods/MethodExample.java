835cbcd577e203f7cb5d53fb7c13607116c3e32a
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-07-30 20:52:56 +0530

    MethodExample.java

---
+++ 07-Methods/MethodExample.java
@@ -0,0 +1,15 @@
+public class MethodExample {
+
+    static void greet() {
+
+        System.out.println("Welcome to Java");
+
+    }
+
+    public static void main(String[] args) {
+
+        greet();
+
+    }
+
+}

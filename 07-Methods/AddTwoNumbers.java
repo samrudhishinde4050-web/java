9bfeee4897e37aff8964f644f8f18616a8bf11a5
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-07-30 20:56:01 +0530

    AddTwoNumbers.java

---
+++ 07-Methods/AddTwoNumbers.java
@@ -0,0 +1,18 @@
+public class AddTwoNumbers {
+
+    // Method to add two numbers
+    static int add(int a, int b) {
+
+        return a + b;
+
+    }
+
+    public static void main(String[] args) {
+
+        int result = add(10, 20);
+
+        System.out.println("Sum = " + result);
+
+    }
+
+}

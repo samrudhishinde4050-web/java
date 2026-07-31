bd10f869d1710b0c0a9f691c5e4067f4be5a7707
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-01 00:53:12 +0530

    ToUpperCaseMethod.java

---
+++ 09-Strings/ToUpperCaseMethod.java
@@ -0,0 +1,11 @@
+public class ToUpperCaseMethod {
+
+    public static void main(String[] args) {
+
+        String name = "java programming";
+
+        System.out.println(name.toUpperCase());
+
+    }
+
+}

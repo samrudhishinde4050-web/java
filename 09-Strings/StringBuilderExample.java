cefabfce438a4216a3cc8ae777647025b14368ee
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-01 00:57:37 +0530

    StringBuilderExample.java

---
+++ 09-Strings/StringBuilderExample.java
@@ -0,0 +1,13 @@
+public class StringBuilderExample {
+
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder("Java");
+
+        sb.append(" Programming");
+
+        System.out.println(sb);
+
+    }
+
+}

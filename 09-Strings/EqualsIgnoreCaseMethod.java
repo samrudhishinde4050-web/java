347ada5e37c49b767842bc757622e8332f218a5d
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-01 00:54:29 +0530

    EqualsIgnoreCaseMethod.java

---
+++ 09-Strings/EqualsIgnoreCaseMethod.java
@@ -0,0 +1,12 @@
+public class EqualsIgnoreCaseMethod {
+
+    public static void main(String[] args) {
+
+        String s1 = "JAVA";
+        String s2 = "java";
+
+        System.out.println(s1.equalsIgnoreCase(s2));
+
+    }
+
+}

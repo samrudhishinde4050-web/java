94ce6235b853c15f271fb2785a85ea3980fbe66f
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-01 00:58:02 +0530

    StringBufferExample.java

---
+++ 09-Strings/StringBufferExample.java
@@ -0,0 +1,13 @@
+public class StringBufferExample {
+
+    public static void main(String[] args) {
+
+        StringBuffer sb = new StringBuffer("Hello");
+
+        sb.append(" World");
+
+        System.out.println(sb);
+
+    }
+
+}

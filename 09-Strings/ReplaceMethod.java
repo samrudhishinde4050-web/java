c2a716a7da4504ab29fbe759565d73ec75bcfd60
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-01 00:56:06 +0530

    ReplaceMethod.java

---
+++ 09-Strings/ReplaceMethod.java
@@ -0,0 +1,11 @@
+public class ReplaceMethod {
+
+    public static void main(String[] args) {
+
+        String text = "Java";
+
+        System.out.println(text.replace("Java", "Python"));
+
+    }
+
+}

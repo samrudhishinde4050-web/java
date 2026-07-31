3bcd974f84c99267823dd1274f25c1c3ea482f93
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-01 00:53:39 +0530

    ToLowerCaseMethod.java

---
+++ 09-Strings/ToLowerCaseMethod.java
@@ -0,0 +1,11 @@
+public class ToLowerCaseMethod {
+
+    public static void main(String[] args) {
+
+        String name = "JAVA PROGRAMMING";
+
+        System.out.println(name.toLowerCase());
+
+    }
+
+}

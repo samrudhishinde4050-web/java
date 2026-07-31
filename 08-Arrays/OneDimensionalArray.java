fe1621341c51e345b8c66e67330c7b52a164deaa
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-07-31 19:25:55 +0530

    OneDimensionalArray.java

---
+++ 08-Arrays/OneDimensionalArray.java
@@ -0,0 +1,15 @@
+public class OneDimensionalArray {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            System.out.println(numbers[i]);
+
+        }
+
+    }
+
+}

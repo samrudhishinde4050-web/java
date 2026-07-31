970376fa617f90bc327ad4badc69a5f0891715a4
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-01 00:54:05 +0530

    EqualsMethod.java

---
+++ 09-Strings/EqualsMethod.java
@@ -0,0 +1,12 @@
+public class EqualsMethod {
+
+    public static void main(String[] args) {
+
+        String s1 = "Java";
+        String s2 = "Java";
+
+        System.out.println(s1.equals(s2));
+
+    }
+
+}

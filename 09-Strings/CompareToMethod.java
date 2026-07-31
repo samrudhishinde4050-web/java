17bf664148a00d2d0c1acdad7fcf5112a2af92f1
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-01 00:54:53 +0530

    CompareToMethod.java

---
+++ 09-Strings/CompareToMethod.java
@@ -0,0 +1,12 @@
+public class CompareToMethod {
+
+    public static void main(String[] args) {
+
+        String s1 = "Apple";
+        String s2 = "Banana";
+
+        System.out.println(s1.compareTo(s2));
+
+    }
+
+}

b1839adb7119f4f05ba90c0e2b1350e9f0b12b48
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-07-31 19:26:50 +0530

    ArrayTraversal.java

---
+++ 08-Arrays/ArrayTraversal.java
@@ -0,0 +1,15 @@
+public class ArrayTraversal {
+
+    public static void main(String[] args) {
+
+        String[] fruits = {"Apple", "Banana", "Mango", "Orange"};
+
+        for (String fruit : fruits) {
+
+            System.out.println(fruit);
+
+        }
+
+    }
+
+}

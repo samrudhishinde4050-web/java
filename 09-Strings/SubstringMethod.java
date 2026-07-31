ef32374ec3a4ec316bc0ca17b959039eadcb4f0f
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-01 00:55:41 +0530

    SubstringMethod.java

---
+++ 09-Strings/SubstringMethod.java
@@ -0,0 +1,11 @@
+public class SubstringMethod {
+
+    public static void main(String[] args) {
+
+        String text = "Java Programming";
+
+        System.out.println(text.substring(5));
+
+    }
+
+}

2d1e3118a451e05fbe14aab7d8c41576e24d270c
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-01 00:55:17 +0530

    ContainsMethod.java

---
+++ 09-Strings/ContainsMethod.java
@@ -0,0 +1,11 @@
+public class ContainsMethod {
+
+    public static void main(String[] args) {
+
+        String text = "Java Programming";
+
+        System.out.println(text.contains("Java"));
+
+    }
+
+}

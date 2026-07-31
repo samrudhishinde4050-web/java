f818f484c6b627f9dc875e5182966ebc9ca93ff9
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-01 00:52:22 +0530

    LengthMethod.java

---
+++ 09-Strings/LengthMethod.java
@@ -0,0 +1,11 @@
+public class LengthMethod {
+
+    public static void main(String[] args) {
+
+        String name = "Java Programming";
+
+        System.out.println("Length = " + name.length());
+
+    }
+
+}

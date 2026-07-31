0c138c439e10c44451cff4fc0b6bba9c741a2b80
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-01 00:56:32 +0530

    TrimMethod.java

---
+++ 09-Strings/TrimMethod.java
@@ -0,0 +1,11 @@
+public class TrimMethod {
+
+    public static void main(String[] args) {
+
+        String text = "   Java Programming   ";
+
+        System.out.println(text.trim());
+
+    }
+
+}

1ff5b51f0b8c60ab026835bf1002e03e3972c365
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-01 00:57:11 +0530

    StringComparison.java

---
+++ 09-Strings/StringComparison.java
@@ -0,0 +1,16 @@
+public class StringComparison {
+
+    public static void main(String[] args) {
+
+        String s1 = "Java";
+        String s2 = "Java";
+        String s3 = "Python";
+
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
+
+        System.out.println(s1.compareTo(s3));
+
+    }
+
+}

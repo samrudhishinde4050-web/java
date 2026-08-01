224edc04db98856305191692833173b89ea0d70f
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-01 17:25:05 +0530

    ArrayDeclaration.java

---
+++ 08-Arrays/ArrayDeclaration.java
@@ -0,0 +1,11 @@
+public class ArrayDeclaration {
+
+    public static void main(String[] args) {
+
+        int[] numbers;
+
+        System.out.println("Array Declared Successfully");
+
+    }
+
+}

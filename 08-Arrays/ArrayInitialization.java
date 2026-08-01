7483dd84fb44cb64672eacce5186884149e2d022
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-01 17:25:30 +0530

    ArrayInitialization.java

---
+++ 08-Arrays/ArrayInitialization.java
@@ -0,0 +1,12 @@
+public class ArrayInitialization {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10,20,30,40,50};
+
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+
+    }
+
+}

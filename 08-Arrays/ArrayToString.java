46a3e3fadffd13464f35c19ec0dcdd1d3b25280e
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-02 23:31:44 +0530

    ArrayToString.java

---
+++ 08-Arrays/ArrayToString.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class ArrayToString {
+
+    public static void main(String[] args) {
+
+        int[] numbers={10,20,30,40};
+
+        System.out.println(Arrays.toString(numbers));
+
+    }
+
+}

40404350b5385d50a952c31720ede7f93e9b0e1c
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-02 23:31:19 +0530

    ArrayEquals.java

---
+++ 08-Arrays/ArrayEquals.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class ArrayEquals {
+
+    public static void main(String[] args) {
+
+        int[] a={10,20,30};
+        int[] b={10,20,30};
+
+        System.out.println(Arrays.equals(a,b));
+
+    }
+
+}

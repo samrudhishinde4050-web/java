48ec28fec9572d09ded09c9a74132795d41f59d3
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-02 23:30:47 +0530

    ArrayFill.java

---
+++ 08-Arrays/ArrayFill.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class ArrayFill {
+
+    public static void main(String[] args) {
+
+        int[] numbers=new int[5];
+
+        Arrays.fill(numbers,100);
+
+        System.out.println(Arrays.toString(numbers));
+
+    }
+
+}

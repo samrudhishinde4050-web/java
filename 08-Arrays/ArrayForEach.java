ffe799fd9a62adde2dc5130affe972c22e46125c
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-02 23:28:34 +0530

    ArrayForEach.java

---
+++ 08-Arrays/ArrayForEach.java
@@ -0,0 +1,15 @@
+public class ArrayForEach {
+
+    public static void main(String[] args) {
+
+        int[] numbers={10,20,30,40,50};
+
+        for(int number:numbers){
+
+            System.out.println(number);
+
+        }
+
+    }
+
+}

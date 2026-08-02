15948c0a5f863c05e7e6a04f2a6e9579be24d953
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-02 23:29:02 +0530

    ArraySort.java

---
+++ 08-Arrays/ArraySort.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class ArraySort {
+
+    public static void main(String[] args) {
+
+        int[] numbers={50,20,40,10,30};
+
+        Arrays.sort(numbers);
+
+        System.out.println(Arrays.toString(numbers));
+
+    }
+
+}

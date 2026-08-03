f8fb1787200d7e1547edf6732dec3a394da0f1d1
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-03 23:01:43 +0530

    ConstructorExample.java

---
+++ 10-OOP/ConstructorExample.java
@@ -0,0 +1,19 @@
+class Student {
+
+    Student() {
+
+        System.out.println("Constructor Called");
+
+    }
+
+}
+
+public class ConstructorExample {
+
+    public static void main(String[] args) {
+
+        Student s = new Student();
+
+    }
+
+}

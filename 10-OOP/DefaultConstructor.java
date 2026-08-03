e436c37fb646949f191d607767ea03e315681107
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-03 23:07:48 +0530

    DefaultConstructor.java

---
+++ 10-OOP/DefaultConstructor.java
@@ -0,0 +1,19 @@
+class Student {
+
+    Student() {
+
+        System.out.println("Default Constructor");
+
+    }
+
+}
+
+public class DefaultConstructor {
+
+    public static void main(String[] args) {
+
+        Student s = new Student();
+
+    }
+
+}

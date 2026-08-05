34ffe5eb2b47d0471f057a661da516bfbee04d19
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-05 21:41:47 +0530

    SuperKeyword.java

---
+++ 10-OOP/SuperKeyword.java
@@ -0,0 +1,15 @@
+class Student {
+
+    static String college = "ABC College";
+
+}
+
+public class StaticKeyword {
+
+    public static void main(String[] args) {
+
+        System.out.println(Student.college);
+
+    }
+
+}

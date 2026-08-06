fd09c0b7be9bda738bb2478a2d550f0c53961736
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-06 16:06:37 +0530

    TryCatchExample.java

---
+++ 11-Exception-Handling/TryCatchExample.java
@@ -0,0 +1,19 @@
+public class TryCatchExample {
+
+    public static void main(String[] args) {
+
+        try {
+
+            int result = 10 / 0;
+
+            System.out.println(result);
+
+        } catch (ArithmeticException e) {
+
+            System.out.println("Cannot divide by zero.");
+
+        }
+
+    }
+
+}

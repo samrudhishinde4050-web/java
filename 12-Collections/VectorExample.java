de38fe4997956dad17e1af73df876a4604bd36c3
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-07 23:43:21 +0530

    VectorExample.java

---
+++ 12-Collections/VectorExample.java
@@ -0,0 +1,17 @@
+import java.util.Vector;
+
+public class VectorExample {
+
+    public static void main(String[] args) {
+
+        Vector<Integer> numbers = new Vector<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+
+        System.out.println(numbers);
+
+    }
+
+}

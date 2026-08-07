05a3977f05d3e255ca6655b1b89f814c24c27cd8
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-07 23:44:29 +0530

    QueueExample.java

---
+++ 12-Collections/QueueExample.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueExample {
+
+    public static void main(String[] args) {
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+
+        System.out.println("Queue: " + queue);
+
+        System.out.println("Removed: " + queue.poll());
+
+        System.out.println("After removal: " + queue);
+
+    }
+
+}

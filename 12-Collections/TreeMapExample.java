2b369125f4b806db929a6c2723ac9121a8f6fef7
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-07 23:47:23 +0530

    TreeMapExample.java

---
+++ 12-Collections/TreeMapExample.java
@@ -0,0 +1,17 @@
+import java.util.TreeMap;
+
+public class TreeMapExample {
+
+    public static void main(String[] args) {
+
+        TreeMap<Integer, String> students = new TreeMap<>();
+
+        students.put(103, "Vaishnavi");
+        students.put(101, "Samrudhi");
+        students.put(102, "Rutuja");
+
+        System.out.println(students);
+
+    }
+
+}

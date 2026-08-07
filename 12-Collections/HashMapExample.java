d7704f04070991399810816bb0095878bc7bdc18
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-07 23:46:32 +0530

    HashMapExample.java

---
+++ 12-Collections/HashMapExample.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class HashMapExample {
+
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> students = new HashMap<>();
+
+        students.put(101, "Samrudhi");
+        students.put(102, "Rutuja");
+        students.put(103, "Vaishnavi");
+
+        System.out.println(students);
+
+        System.out.println("Student 101: " + students.get(101));
+
+    }
+
+}

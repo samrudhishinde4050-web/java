aeb05cbd1b4e174dd35f2a7308cb3222359c7779
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-07 23:42:24 +0530

    LinkedListExample.java

---
+++ 12-Collections/LinkedListExample.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+public class LinkedListExample {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> names = new LinkedList<>();
+
+        names.add("Samrudhi");
+        names.add("Rutuja");
+        names.add("Vaishnavi");
+
+        System.out.println(names);
+
+    }
+
+}

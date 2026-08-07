2fcd3cedb5b1eed1a2c9c7e0c335f34bd0cf4059
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-07 23:45:44 +0530

    LinkedHashSetExample.java

---
+++ 12-Collections/LinkedHashSetExample.java
@@ -0,0 +1,18 @@
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetExample {
+
+    public static void main(String[] args) {
+
+        LinkedHashSet<String> names = new LinkedHashSet<>();
+
+        names.add("Samrudhi");
+        names.add("Rutuja");
+        names.add("Vaishnavi");
+        names.add("Samrudhi");
+
+        System.out.println(names);
+
+    }
+
+}

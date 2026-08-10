4ae6f25e0ac08ccb3016928210083adf31044918
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-10 13:36:30 +0530

    CreateFile.java

---
+++ 14-File-Handling/CreateFile.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.IOException;
+
+public class CreateFile {
+
+    public static void main(String[] args) {
+
+        File file = new File("sample.txt");
+
+        try {
+
+            if (file.createNewFile()) {
+
+                System.out.println("File created successfully.");
+
+            } else {
+
+                System.out.println("File already exists.");
+
+            }
+
+        } catch (IOException e) {
+
+            System.out.println("An error occurred.");
+
+        }
+
+    }
+
+}

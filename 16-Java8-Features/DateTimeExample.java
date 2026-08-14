c5b34c6f00ba1b9715adf7828f3d7caa982d1295
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-14 20:28:02 +0530

    DateTimeExample.java

---
+++ 16-Java8-Features/DateTimeExample.java
@@ -0,0 +1,22 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class DateTimeExample {
+
+    public static void main(String[] args) {
+
+        LocalDate date = LocalDate.now();
+
+        LocalTime time = LocalTime.now();
+
+        LocalDateTime dateTime =
+                LocalDateTime.now();
+
+        System.out.println("Date: " + date);
+        System.out.println("Time: " + time);
+        System.out.println("Date and Time: " + dateTime);
+
+    }
+
+}

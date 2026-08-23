aab9ec76806ea552bec0d56054e0ec57244c96a7
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-23 21:38:29 +0530

    Encapsulation.java

---
+++ 18-OOP-Advanced/Encapsulation.java
@@ -0,0 +1,44 @@
+class Student {
+
+    private String name;
+    private int age;
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setAge(int age) {
+
+        this.age = age;
+    }
+
+    public int getAge() {
+
+        return age;
+    }
+}
+
+public class Encapsulation {
+
+    public static void main(String[] args) {
+
+        Student student = new Student();
+
+        student.setName("Samrudhi");
+        student.setAge(21);
+
+        System.out.println(
+                "Name: " + student.getName()
+        );
+
+        System.out.println(
+                "Age: " + student.getAge()
+        );
+    }
+}

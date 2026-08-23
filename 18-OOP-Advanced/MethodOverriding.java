fe34e83e24d8c38dd13e58036e80df07a4341342
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-23 21:34:31 +0530

    MethodOverriding.java

---
+++ 18-OOP-Advanced/MethodOverriding.java
@@ -0,0 +1,24 @@
+class Animal {
+
+    void sound() {
+        System.out.println("Animal makes a sound.");
+    }
+}
+
+class Dog extends Animal {
+
+    @Override
+    void sound() {
+        System.out.println("Dog barks.");
+    }
+}
+
+public class MethodOverriding {
+
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+
+        dog.sound();
+    }
+}

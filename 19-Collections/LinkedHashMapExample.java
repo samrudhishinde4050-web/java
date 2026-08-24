54492d95db15b0334d498b9abc09d764f721026c
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-24 11:35:29 +0530

    LinkedHashMapExample.java

---
+++ 19-Collections/LinkedHashMapExample.java
@@ -0,0 +1,16 @@
+import java.util.LinkedHashMap;
+
+public class LinkedHashMapExample {
+
+    public static void main(String[] args) {
+
+        LinkedHashMap<Integer, String> students =
+                new LinkedHashMap<>();
+
+        students.put(101, "Samrudhi");
+        students.put(102, "Rutuja");
+        students.put(103, "Vaishnavi");
+
+        System.out.println(students);
+    }
+}

7c73573aba48f8521c2385fb8fc61d51e85dc1ac
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-26 18:25:59 +0530

    FileReaderExample.java

---
+++ 21-File-Handling/FileReaderExample.java
@@ -0,0 +1,29 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderExample {
+
+    public static void main(String[] args) {
+
+        try {
+
+            FileReader reader =
+                    new FileReader("student.txt");
+
+            int character;
+
+            while ((character = reader.read()) != -1) {
+
+                System.out.print(
+                        (char) character
+                );
+            }
+
+            reader.close();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+}

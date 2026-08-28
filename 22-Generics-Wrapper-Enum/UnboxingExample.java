bb8d0a8722ac9eedd0a48d033b7bff2de8c034b6
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-28 22:35:20 +0530

    UnboxingExample.java

---
+++ 22-Generics-Wrapper-Enum/UnboxingExample.java
@@ -0,0 +1,12 @@
+public class UnboxingExample {
+
+    public static void main(String[] args) {
+
+        Integer value = 100;
+
+        int number = value;
+
+        System.out.println("Wrapper: " + value);
+        System.out.println("Primitive: " + number);
+    }
+}

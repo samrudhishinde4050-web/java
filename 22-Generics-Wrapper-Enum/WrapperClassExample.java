8cba0bc92f2d53c6ee93a19c288c1393ba63ed3e
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-28 22:34:18 +0530

    WrapperClassExample.java

---
+++ 22-Generics-Wrapper-Enum/WrapperClassExample.java
@@ -0,0 +1,15 @@
+public class WrapperClassExample {
+
+    public static void main(String[] args) {
+
+        Integer number = 100;
+        Double price = 99.50;
+        Character grade = 'A';
+        Boolean status = true;
+
+        System.out.println("Integer: " + number);
+        System.out.println("Double: " + price);
+        System.out.println("Character: " + grade);
+        System.out.println("Boolean: " + status);
+    }
+}

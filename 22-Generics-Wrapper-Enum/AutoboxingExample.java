f5bb04a9c857e64fa819627bb4860707dd9c2e72
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-08-28 22:34:49 +0530

    AutoboxingExample.java

---
+++ 22-Generics-Wrapper-Enum/AutoboxingExample.java
@@ -0,0 +1,12 @@
+public class AutoboxingExample {
+
+    public static void main(String[] args) {
+
+        int number = 50;
+
+        Integer value = number;
+
+        System.out.println("Primitive: " + number);
+        System.out.println("Wrapper: " + value);
+    }
+}

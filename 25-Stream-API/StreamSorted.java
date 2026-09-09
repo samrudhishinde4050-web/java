16f9e4e6b8ee88522c14a499aa5ce96ce80a89c7
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-09-09 18:54:26 +0530

    StreamSorted.java

---
+++ 25-Stream-API/StreamSorted.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamSorted {
+    public static void main(String[] args) {
+
+        List<Integer> numbers =
+                Arrays.asList(50, 10, 40, 20, 30);
+
+        numbers.stream()
+                .sorted()
+                .forEach(System.out::println);
+    }
+}

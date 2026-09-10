630714409e3fa0fb694ed87492de84527c1da7ca
Samrudhi Bajirao Shinde <samrudhishinde4050@gmail.com>
2026-09-10 19:33:47 +0530

    LocalDateTimeExample.java

---
+++ 26-Date-Time-API/LocalDateTimeExample.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+
+public class LocalDateTimeExample {
+    public static void main(String[] args) {
+
+        LocalDateTime dateTime = LocalDateTime.now();
+
+        System.out.println("Date and Time: " + dateTime);
+    }
+}
